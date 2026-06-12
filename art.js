2b5253e38bfb559b8c498f941b5ed629ecdd3fa4
Elizaveta Grushnikova <46537176+grushnik@users.noreply.github.com>
2026-06-12 12:30:54 -0500

    Add artworks array with various art projects

---
+++ art.js
@@ -0,0 +1,58 @@
+const artworks = [
+  {
+    title: "Earth Day",
+    date: "Kindergarten, 2025",
+    theme: "school",
+    image: "images/earth-day.jpg",
+    note: "Emmett holding the Earth."
+  },
+  {
+    title: "Mom, I Love You to Pieces",
+    date: "Mother’s Day, 2025",
+    theme: "family",
+    image: "images/mom-love-you-to-pieces.jpg",
+    note: "A sweet Mother’s Day project with a puzzle frame."
+  },
+  {
+    title: "Dr. Seuss Craft",
+    date: "Dr. Seuss Week",
+    theme: "school",
+    image: "images/dr-seuss.jpg",
+    note: "Emmett as Dr. Seuss."
+  },
+  {
+    title: "Happy St. Patrick’s Day",
+    date: "March 2025",
+    theme: "holiday",
+    image: "images/st-patricks-day.jpg",
+    note: "Rainbow, pot of gold, and holiday colors."
+  },
+  {
+    title: "Frog Coloring Page",
+    date: "Kindergarten",
+    theme: "animals",
+    image: "images/frog.jpg",
+    note: "A very happy green frog."
+  },
+  {
+    title: "Flower CVC Words",
+    date: "Kindergarten",
+    theme: "school",
+    image: "images/cvc-flower.jpg",
+    note: "Learning short words with a flower activity."
+  },
+  {
+    title: "Halloween House",
+    date: "October 2025",
+    theme: "holiday",
+    image: "images/halloween-house.jpg",
+    note: "Pumpkins, bats, and a spooky little house."
+  },
+  {
+    title: "Free Drawing",
+    date: "Kindergarten",
+    theme: "drawing",
+    image: "images/free-drawing.jpg",
+    note: "One of Emmett’s original pencil drawings."
+  }
+];
